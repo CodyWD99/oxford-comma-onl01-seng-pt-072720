322e6ad6b9e0dfd7df9086f25745c44914401bab
Cody Dupuis <codydupuis99@gmail.com>
2020-07-31 10:25:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,13 @@ array = ["Milk", "Eggs", "Bread"]
 
 def oxford_comma(array)
   if array.length == 2 
-    array[-2] << ", and "
+    array[0] << ", and "
     array.join
   else array.length == 1
-    
+    array
+  else array.length > 2
+    array[-2] >> ", and "
+  end
+end
+
+oxford_comma(array)
