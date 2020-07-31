4b2edc82e99eb4ae56fc2eb4964d10aeb338feaf
Cody Dupuis <codydupuis99@gmail.com>
2020-07-31 10:26:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,9 @@ def oxford_comma(array)
   if array.length == 2 
     array[0] << ", and "
     array.join
-  else array.length == 1
+  elsif array.length == 1
     array
-  else array.length > 2
+  elsif array.length > 2
     array[-2] >> ", and "
   end
 end
